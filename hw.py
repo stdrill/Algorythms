e206212cc2b485c1efa61ee12d806d21e139b31b
Tatiana Stavtseva <109695593+stdrill@users.noreply.github.com>
2023-05-06 11:42:38 +0300

    red-black tree

--- hw.py
+++ hw.py
@@ -1,105 +1,127 @@
-class Node:
-    def __init__(self, data):
-        self.next = None
-        self.prev = None
-        self.data = data
-
-    def __str__(self):
-        return str(self.data)
-
-
-class MyList:
-    def __init__(self, lst=None):
-        self.head = None
-        self.tail = None
-        if lst is not None:
-            for i in lst:
-                self.add(i)
-
-    def add(self, value):
-        obj = Node(value)
-        current_tail = self.tail
-        self.tail = obj
-        if self.head is None:
-            self.head = obj
-            return
-        obj.prev = current_tail
-        current_tail.next = obj
-
-    def __len__(self):
-        count = 0
-        obj = self.head
-        while obj is not None:
-            count += 1
-            obj = obj.next
-        return count
-
-    def __str__(self):
-        current = self.head
-        lst = []
-        while current is not None:
-            lst.append(str(current.data))
-            current = current.next
-        return " ".join(lst)
-
-    def __getitem__(self, item):
-        if type(item) != int:
-            raise IndexError('неверный индекс')
-        if item < 0:
-            raise IndexError('неверный индекс')
-        start = 0
-        obj = self.head
-        while start < item:
-            if obj is None:
-                raise IndexError('неверный индекс')
-            obj = obj.next
-            start += 1
-        if obj is None:
-            raise IndexError('неверный индекс')
-        return obj
-
-    def reverse(self):
-        current = self.head
-        while current is not None:
-            current.next, current.prev = current.prev, current.next
-            current = current.prev
-        self.head, self.tail = self.tail, self.head
-
-    def sort(self, reverse=False):
-        len_list = len(self)
-        for i in range(len_list):
-            current = self[0]
-            j = 0
-            next_obj = current.next
-            if next_obj is None:
-                self.tail = current
-            while next_obj is not None and j < len_list - 1 - i:
-                j+=1
-                if current.data > next_obj.data:
-                    if current == self.head:
-                        self.head = next_obj
-                    if next_obj == self.tail:
-                        self.tail = current
-                    if current.prev is not None:
-                        current.prev.next = next_obj
-                    if next_obj.next is not None:
-                        next_obj.next.prev = current
-                    current.prev, current.next, next_obj.prev, next_obj.next = next_obj, next_obj.next, current.prev, current
-                else:
-                    current = next_obj
-
-                next_obj = current.next
-        if reverse:
-            self.reverse()
-
-
 def ex():
-    lst = MyList([1, 2, 6, 2, 9, 3, 5, 1])
-    print(lst)
-    lst.reverse()
-    print(lst)
-    lst.sort()
-    print(lst)
+    tree = Tree()
+    tree.add(1)
+    tree.add(5)
+    tree.add(8)
+    tree.add(4)
+    tree.add(3)
+    tree.add(10)
+    tree.add(7)
+    tree.print()
+
+
+class Node:
+    def __init__(self):
+        self.value: int = None
+        self.isRed: bool = False
+        self.left: Node = None
+        self.right: Node = None
+
+
+class Tree:
+    def __init__(self):
+        self.root: Node = None
+
+    def print(self):
+        self.print_node(self.root)
+
+    def add(self, value: int):
+        result = True
+
+        if self.root is not None:
+            result = self.add_note(self.root, value)
+            self.root = self.rebalancing(self.root)
+            self.root.isRed = False
+        else:
+            self.root = Node()
+            self.root.isRed = False
+            self.root.value = value
+
+        return True
+
+    def rebalancing(self, node) -> Node:
+        result: Node = node
+        need_rebalancing = True
+
+        while need_rebalancing:
+
+            need_rebalancing = False
+
+            if (result.right is not None and result.right.isRed and
+                    (result.left is None or not result.left.isRed)):
+                need_rebalancing = True
+                result = self.right_swap(result)
+
+            if (result.left is not None and result.left.isRed and
+                    result.left.left is not None and result.left.left.isRed):
+                need_rebalancing = True
+                result = self.left_swap(result)
+
+            if (result.left is not None and result.left.isRed and
+                    result.right is not None and result.right.isRed):
+                need_rebalancing = True
+                self.color_swap(result)
+
+        return result
+
+    def right_swap(self, node: Node) -> Node:
+        right = node.right
+        between = right.left
+        right.left = node
+        node.right = between
+        right.isRed = node.isRed
+        node.isRed = True
+        return right
+
+    def left_swap(self, node: Node) -> Node:
+        left = node.left
+        between = left.right
+        left.right = node
+        node.left = between
+        left.isRed = node.isRed
+        node.isRed = True
+        return left
+
+    def color_swap(self, node: Node):
+        node.right.isRed = False
+        node.left.isRed = False
+        node.isRed = True
+
+    def add_note(self, node: Node, value: int):
+
+        if (node.value == value):
+            return False
+
+        result: bool = True
+
+        if node.value > value:
+            if node.left is not None:
+                result = self.add_note(node.left, value)
+                node.left = self.rebalancing(node.left)
+            else:
+                node.left = Node()
+                node.left.isRed = True
+                node.left.value = value
+                result = True
+        else:
+            if (node.right is not None):
+                result = self.add_note(node.right, value)
+                node.right = self.rebalancing(node.right)
+            else:
+                node.right = Node()
+                node.right.isRed = True
+                node.right.value = value
+                result = True
+
+        return result
+
+    def print_node(self, node: Node):
+        if node is None:
+            return
+        self.print_node(node.left)
+        print(node.value)
+        self.print_node(node.right)
+
 
 if __name__ == '__main__':
     ex()
